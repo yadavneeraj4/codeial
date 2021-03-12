4c0b1e050d60ac88fa2696486ec68ebc2e85eacb
Neeraj <raoneeraj4@gmail.com>
2021-03-12 14:17:16 +0530

    populated the user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,10 +3,18 @@ const Post =require('../models/post');
 module.exports.home=function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',25);
-    Post.find({},function(err,posts){
-        return res.render('home',{
-            title:"Codeial | Home",
-            posts: posts
-    });
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts: posts
+    // });
+//});
+
+//populate the user of each post
+Post.find({}).populate('user').exec(function(err,posts){
+    return res.render('home',{
+        title:"Codeial | Home",
+        posts: posts
 });
+})
 }
