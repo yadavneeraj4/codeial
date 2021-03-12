09b7ed28f5f78a2d94cfebc8d7b40216fc94879f
Neeraj <raoneeraj4@gmail.com>
2021-03-12 13:36:52 +0530

    rendering of posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,12 @@
+const Post =require('../models/post');
+
 module.exports.home=function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-return res.render('home',{
-    title:"Home"
-})
-}
-module.exports.image=function(req,res){
-    return res.end('all images');
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+    Post.find({},function(err,posts){
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts: posts
+    });
+});
 }
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/',homeController.home);
-router.get('/image',homeController.image);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 //for any further routes access from here
 //router.use('/routerName',require(./routerFile));
 
