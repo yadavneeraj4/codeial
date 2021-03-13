d236ff7b87ad384823b2471efd82e07a467fc381
Neeraj <raoneeraj4@gmail.com>
2021-03-13 19:24:51 +0530

    deletion of posts and comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -20,3 +20,21 @@ module.exports.create = function(req,res){
         }
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id, function(err,comment){
+        // .id means converting the object id into string
+        if(comment.user == req.user.id){
+
+            let postId=comment.post;
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err,post){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
