c99fa4e27dec20b00f661592be986612cbf55da1
Neeraj <raoneeraj4@gmail.com>
2021-03-17 16:53:31 +0530

    added flash messages

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -16,11 +16,12 @@ module.exports.create = async function(req, res){
             post.comments.push(comment);
             post.save();
 
+            req.flash('success','Comment Published!');
             res.redirect('/');
         }
     }catch(err){
-        console.log('Error', err);
-        return;
+        req.flash('error',err);
+        return res.redirect('/');
     }
     
 }
@@ -39,12 +40,14 @@ module.exports.destroy = async function(req, res){
 
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
 
+            req.flash('success','Comment Deleted!')
             return res.redirect('back');
         }else{
+            req.flash('error','You cannot delete this comment');
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error', err);
+        req.flash('error',err);
         return;
     }
     
